da8bc8afef7248b682e9d4c294c223ae9eefb944
Andrii Buga <buga@DESKTOP-1HE2HAF.localdomain>
2023-10-14 14:13:34 +0200

    preproc for audios

--- testing/audio_to_spec.py
+++ testing/audio_to_spec.py
@@ -1,40 +1,99 @@
+# import librosa
+# import librosa.display
+# import matplotlib.pyplot as plt
+# import numpy as np
+# import os
+# import noisereduce as nr
+#
+#
+# def process_audio_and_create_spectrogram(audio_file_path, spectrogram_folder):
+#     try:
+#         y, sr = librosa.load(audio_file_path, sr=None)
+#         y_denoised = nr.reduce_noise(y=y, sr=sr)
+#         y_trimmed, index = librosa.effects.trim(y_denoised, top_db=20)
+#         spectrogram = np.abs(librosa.stft(y_trimmed))
+#         plt.figure(figsize=(10, 6))
+#         librosa.display.specshow(librosa.amplitude_to_db(spectrogram, ref=np.max), sr=sr, x_axis='time', y_axis='log')
+#         plt.colorbar(format='%+2.0f dB')
+#         plt.title(f'Spectrogram of {os.path.basename(audio_file_path)}')
+#
+#         plt.savefig(os.path.join(spectrogram_folder, f"{os.path.splitext(os.path.basename(audio_file_path))[0]}.png"))
+#         plt.close()
+#     except Exception as e:
+#         print(f"Error processing {audio_file_path}: {e}")
+#
+#
+# audio_folder = "audios"
+#
+# categories = ["belly_pain", "discomfort", "hungry", "tired", "burping"]
+#
+# for category in categories:
+#     category_path = os.path.join(audio_folder, category)
+#     spectrogram_folder = os.path.join(category_path, "spectrograms")
+#     os.makedirs(spectrogram_folder, exist_ok=True)
+#
+#     for filename in os.listdir(category_path):
+#         if not filename.endswith(".wav"):
+#             continue
+#
+#         file_path = os.path.join(category_path, filename)
+#         process_audio_and_create_spectrogram(file_path, spectrogram_folder)
 import librosa
 import librosa.display
-import matplotlib.pyplot as plt
 import numpy as np
 import os
-import noisereduce as nr
+import matplotlib.pyplot as plt
 
 
-def process_audio_and_create_spectrogram(audio_file_path, spectrogram_folder):
+def audio_to_spec(clean_audio, spectrogram_folder):
     try:
-        y, sr = librosa.load(audio_file_path, sr=None)
-        y_denoised = nr.reduce_noise(y=y, sr=sr)
-        y_trimmed, index = librosa.effects.trim(y_denoised, top_db=20)
-        spectrogram = np.abs(librosa.stft(y_trimmed))
+        y, sr = librosa.load(clean_audio, sr=None)
+        spectrogram = np.abs(librosa.stft(y))
+
         plt.figure(figsize=(10, 6))
         librosa.display.specshow(librosa.amplitude_to_db(spectrogram, ref=np.max), sr=sr, x_axis='time', y_axis='log')
         plt.colorbar(format='%+2.0f dB')
-        plt.title(f'Spectrogram of {os.path.basename(audio_file_path)}')
+        plt.title(f'Spectrogram of {os.path.basename(clean_audio)}')
 
-        plt.savefig(os.path.join(spectrogram_folder, f"{os.path.splitext(os.path.basename(audio_file_path))[0]}.png"))
+        # Extract the category from the clean audio path
+        category = os.path.basename(os.path.dirname(clean_audio))
+
+        # Create subfolder path based on the category
+        subfolder = f"spectrogram_{category}"
+        subfolder_path = os.path.join(spectrogram_folder, subfolder)
+        os.makedirs(subfolder_path, exist_ok=True)
+
+        # Save the spectrogram in the subfolder
+        spectrogram_path = os.path.join(subfolder_path,
+                                        f"{os.path.splitext(os.path.basename(clean_audio))[0]}.png")
+        plt.savefig(spectrogram_path)
         plt.close()
+
+        return spectrogram_path
     except Exception as e:
-        print(f"Error processing {audio_file_path}: {e}")
+        print(f"Error creating spectrogram for {clean_audio}: {e}")
+        return None
 
 
-audio_folder = "audios"
+audio_folder = "clean_audio"
 
-categories = ["belly_pain", "discomfort", "hungry", "tired", "burping"]
+categories = [
+    "clean_belly_pain",
+    "clean_discomfort",
+    "clean_hungry",
+    "clean_tired",
+    "clean_burping"
+]
+
+spectrogram_folder = "spectrogram"
+os.makedirs(spectrogram_folder, exist_ok=True)
 
 for category in categories:
     category_path = os.path.join(audio_folder, category)
-    spectrogram_folder = os.path.join(category_path, "spectrograms")
-    os.makedirs(spectrogram_folder, exist_ok=True)
 
     for filename in os.listdir(category_path):
         if not filename.endswith(".wav"):
             continue
 
         file_path = os.path.join(category_path, filename)
-        process_audio_and_create_spectrogram(file_path, spectrogram_folder)
+        audio_to_spec(file_path, spectrogram_folder)
