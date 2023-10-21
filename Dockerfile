0fca24f92592bb6e98992364641e88e28bcd19a7
Florian Schmitt <florian.r.schmitt@gmail.com>
2023-10-21 11:51:24 +0200

    changes in dockerfile and in prediction.py

--- Dockerfile
+++ Dockerfile
@@ -5,4 +5,4 @@ WORKDIR /app
 COPY requirements.txt requirements.txt
 RUN pip install --upgrade pip
 RUN pip install -r requirements.txt
-COPY . .
+COPY api /app
--- api/apps/pages/prediction.py
+++ api/apps/pages/prediction.py
@@ -1,37 +1,46 @@
-from fastapi import APIRouter, Request, File, UploadFile, BackgroundTasks
-from api.apps.render_template import render_template
-from pathlib import Path
-from api.components import forms
+#from fastapi import APIRouter, Request, File, UploadFile, BackgroundTasks
+#from api.apps.render_template import render_template
+#from pathlib import Path
+#from api.components import forms
+from fastapi import FastAPI
+print('Hello')
+#prediction_endpoint = APIRouter()
+api = FastAPI()
+@api.get("/")
+def index():
+    return {"ok": "API connected"}
 
-prediction_endpoint = APIRouter()
+@api.get("/predict")
+def predict():
+    return "Here is your prediction"
 
-
-@prediction_endpoint.get("/")
-async def get_prediction_page(request: Request):
-    return render_template("predict.html", {"request": request})
-
-
-async def save_upload_file(file: UploadFile, destination: Path):
-    try:
-        with destination.open("wb") as buffer:
-            buffer.write(file.file.read())
-    except Exception as e:
-        print(f"Failed to save file: {e}")
-
-
-@prediction_endpoint.post("/")
-async def upload_file(request: Request, background_tasks: BackgroundTasks, file: UploadFile = File(...)):
-    errors = []
-    msg = None
-
-    form = forms.FileUploadForm(request)
-    await form.load_data()
-
-    if await form.file_is_valid():
-        destination = Path(f"data/input_data/audio/{file.filename}")
-        background_tasks.add_task(save_upload_file, file, destination)
-        msg = f"File {file.filename} uploaded successfully!"
-    else:
-        errors = form.errors
-
-    return render_template("predict.html", {"request": request, "errors": errors, "msg": msg})
+#@prediction_endpoint.get("/")
+#async def get_prediction_page(request: Request):
+#    return render_template("predict.html", {"request": request})
+#
+#
+#async def save_upload_file(file: UploadFile, destination: Path):
+#    try:
+#        with destination.open("wb") as buffer:
+#            buffer.write(file.file.read())
+#    except Exception as e:
+#        print(f"Failed to save file: {e}")
+#
+#
+#@prediction_endpoint.post("/")
+#async def upload_file(request: Request, background_tasks: BackgroundTasks, file: UploadFile = File(...)):
+#    errors = []
+#    msg = None
+#
+#    form = forms.FileUploadForm(request)
+#    await form.load_data()
+#
+#    if await form.file_is_valid():
+#        destination = Path(f"data/input_data/audio/{file.filename}")
+#        background_tasks.add_task(save_upload_file, file, destination)
+#        msg = f"File {file.filename} uploaded successfully!"
+#    else:
+#        errors = form.errors
+#
+#    return render_template("predict.html", {"request": request, "errors": errors, "msg": msg})
+#
