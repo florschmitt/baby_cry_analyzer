f60a41160a94f417bc18c753c8cdd4a63a3ae494
Buga Andrii <aladaris88@gmail.com>
2023-10-27 16:53:48 +0200

    presentation and about pages

--- api/apps/pages/about_page.py
+++ api/apps/pages/about_page.py
@@ -6,11 +6,9 @@ about_endpoint = APIRouter()
 
 @about_endpoint.get("/presentation")
 async def get_presentation_page(request: Request):
-    # return {"Work": "In progress"}
-    return render_template("presentation.html", {"request": request})
+    return await render_template("presentation.html", {"request": request})
 
 
 @about_endpoint.get("/about")
 async def get_about_page(request: Request):
-    # return {"Work": "In progress"}
-    return render_template("about.html", {"request": request})
+    return await render_template("about.html", {"request": request})
--- api/apps/pages/forest.py
+++ api/apps/pages/forest.py
@@ -13,7 +13,7 @@ forest_endpoint = APIRouter()
 
 @forest_endpoint.get("/")
 async def get_forest_page(request: Request):
-    return render_template("forest.html", {"request": request})
+    return await render_template("forest.html", {"request": request})
 
 
 @forest_endpoint.post("/")
@@ -30,23 +30,23 @@ async def forest_predict(request: Request, file: UploadFile = File(...)) -> Dict
     form = forms.FileUploadForm(request)
     await form.load_data()
     if not await form.file_is_valid():
-        return render_template("forest.html", {"request": request, "errors": form.errors})
+        return await render_template("forest.html", {"request": request, "errors": form.errors})
 
     # Read the uploaded audio file
     audio_content = await file.read()
     audio_base64 = base64.b64encode(audio_content).decode("utf-8")
     with open("temp_audio_file.wav", "wb") as audio_file:
         audio_file.write(audio_content)
-    mfccs = extract_mfcc("temp_audio_file.wav")
-    prediction = predict(mfccs, get_forest_model())
-    y_clean = load_audio(BytesIO(audio_content))
-    spectrogram = get_spectrogram(y_clean)
-    spectrogram_base64 = image_to_base64(spectrogram)
+    mfccs = await extract_mfcc("temp_audio_file.wav")
+    prediction = await predict(mfccs, get_forest_model())
+    y_clean = await load_audio(BytesIO(audio_content))
+    spectrogram = await get_spectrogram(y_clean)
+    spectrogram_base64 = await image_to_base64(spectrogram)
     prediction_label = class_labels.get(prediction[0])
 
-    return render_template("forest.html", {
+    return await render_template("forest.html", {
         "request": request,
-        "msg": f"{prediction_label}",
+        "msg": "File Loaded",
         "audio_base64": audio_base64,
         "spectrogram": spectrogram_base64,
         "filename": file.filename,
@@ -54,7 +54,7 @@ async def forest_predict(request: Request, file: UploadFile = File(...)) -> Dict
     })
 
 
-def predict(audio_features, model):
+async def predict(audio_features, model):
     features_flatten = audio_features.flatten()
     required_features = 1200
     repeat_times = required_features // len(features_flatten) + 1
